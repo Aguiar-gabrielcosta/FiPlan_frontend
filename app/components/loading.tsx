2cd52b95f459199338cb51d303a14e963cc3453b
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-07-19 17:17:30 -0300

    Adiciona spinners

--- app/components/loading.tsx
+++ app/components/loading.tsx
@@ -1,3 +1,9 @@
+import { BeatLoader } from 'react-spinners'
+
 export default function Loading() {
-  return <p className="text-center">Loading...</p>
+  return (
+    <div className="flex items-center justify-center py-8 text-center text-primaryDR">
+      <BeatLoader color="#0085EA" />
+    </div>
+  )
 }
--- app/components/resumo/expenseInfo.tsx
+++ app/components/resumo/expenseInfo.tsx
@@ -32,9 +32,9 @@ export default function ExpenseInfo({
 
   return (
     <div className="flex items-center gap-4 rounded-lg bg-neutralWhite p-2 drop-shadow-md">
-      <h5 className="w-32 truncate text-nowrap text-start font-bold text-primaryDR">
+      <h4 className="w-32 truncate text-nowrap text-start font-bold text-primaryDR">
         {category}
-      </h5>
+      </h4>
       <div className="h-[10px] w-[150px] rounded-lg bg-primaryLR">
         <div
           className={`h-full rounded-lg ${bgAlertVariants[alert]}`}
