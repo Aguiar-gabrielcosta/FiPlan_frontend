fca6b650ff11858764ec10a6a2bd9745ace48a2a
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-07-20 23:31:30 -0300

    Feature de seleção de planos + url

--- app/components/resumo/planSelectorForm.tsx
+++ app/components/resumo/planSelectorForm.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+
 export type Plan = {
   budgetId: string
   startDate: string
@@ -11,13 +13,34 @@ type Props = {
 }
 
 export default function PlanSelectorForm({ data }: Props) {
+  const urlParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  function selectPlan(planId: string) {
+    const params = new URLSearchParams(urlParams)
+
+    if (planId) {
+      params.set('plan', planId)
+    } else {
+      params.delete('plan')
+    }
+
+    console.log(params.toString())
+    console.log(pathname)
+
+    replace(`${pathname}?${params.toString()}`)
+  }
+
   return (
     <form action="" id="dashboardPlan">
       <select
         form="dashboardPlan"
         className="rounded-lg border border-primaryDR p-2"
         aria-label="Selecione o período de análise"
+        onChange={(e) => selectPlan(e.currentTarget.value)}
       >
+        <option value="">Selecione um plano</option>
         {data.map((plan) => {
           return (
             <option key={plan.budgetId} value={plan.budgetId}>
--- app/resumo/page.tsx
+++ app/resumo/page.tsx
@@ -6,7 +6,13 @@ import PlanSelector from '../components/resumo/planSelector'
 import ExpensesByCategory from '../components/resumo/expensesByCategory'
 import BudgetInfo from '../components/resumo/budgetInfo'
 
-export default function Resumo() {
+export default function Resumo({
+  searchParams,
+}: {
+  searchParams?: { plan?: string }
+}) {
+  const planId = searchParams?.plan || ''
+
   return (
     <div className="flex flex-col gap-2">
       <div className="grid grid-cols-[1fr_auto] gap-2">
