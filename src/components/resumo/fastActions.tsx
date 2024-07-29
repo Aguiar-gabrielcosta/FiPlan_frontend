b6089a511772fcfd0459bd44eac767c97e96ff62
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-07-29 18:01:00 -0300

    Ariciona um transição no hover dos botões

--- src/components/resumo/fastActions.tsx
+++ src/components/resumo/fastActions.tsx
@@ -8,21 +8,21 @@ export default function FastActions() {
     <Section width="fit" title="Acesso Rápido">
       <div className="flex h-full gap-2">
         <Link
-          className="flex flex-col items-center justify-center gap-1 rounded-lg bg-primaryD p-1 font-medium text-neutralWhite hover:bg-primary"
+          className="flex flex-col items-center justify-center gap-1 rounded-lg bg-primaryD p-1 font-medium text-neutralWhite transition-all hover:bg-primary"
           href={'resumo/planejamento/plano'}
         >
           <Route width={48} height={40} />
           <p className="w-[100px] text-center">+ Plano</p>
         </Link>
         <Link
-          className="flex flex-col items-center justify-center gap-1 rounded-lg bg-primaryD p-1 font-medium text-neutralWhite hover:bg-primary"
+          className="flex flex-col items-center justify-center gap-1 rounded-lg bg-primaryD p-1 font-medium text-neutralWhite transition-all hover:bg-primary"
           href={'resumo/planejamento/categoria'}
         >
           <Package width={40} height={40} />
           <p className="w-[100px] text-center">+ Categoria</p>
         </Link>
         <Link
-          className="flex flex-col items-center justify-center gap-1 rounded-lg bg-primaryD p-1 font-medium text-neutralWhite hover:bg-primary"
+          className="flex flex-col items-center justify-center gap-1 rounded-lg bg-primaryD p-1 font-medium text-neutralWhite transition-all hover:bg-primary"
           href={'resumo/planejamento/transacao'}
         >
           <CircleDollarSign width={40} height={40} />
