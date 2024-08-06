29648360407f4f38542847974fc8e68048768b20
Gabriel Aguiar <Aguiar.gc@hotmail.com>
2024-08-06 18:45:12 -0300

    chore: Ajustes de comentários

--- src/app/resumo/planejamento/[id]/categoria/page.tsx
+++ src/app/resumo/planejamento/[id]/categoria/page.tsx
@@ -1,9 +1,20 @@
 export default function EditCategory({ params }: { params: { id: string } }) {
   const categoryId = params.id
 
+  // const category = await Api.categoryById(categoryId)
+
+  // if (!category.data) {
+  //   return (
+  //     <p className="mx-auto mt-32 text-center text-lg font-medium text-alertRed">
+  //       {category.message}
+  //     </p>
+  //   )
+  // }
+
   return (
     <div className="flex min-h-full min-w-full items-center justify-center">
       {categoryId}
+      {/* <EditCategoryForm category={category.data} /> */}
     </div>
   )
 }
--- src/app/resumo/planejamento/[id]/plano/page.tsx
+++ src/app/resumo/planejamento/[id]/plano/page.tsx
@@ -5,20 +5,11 @@ export default async function EditPlan({ params }: { params: { id: string } }) {
   const planId = params.id
 
   const plan = await Api.planById(planId)
-  const categories = await Api.categoriesByPlan(planId)
 
   if (!plan.data) {
     return (
       <p className="mx-auto mt-32 text-center text-lg font-medium text-alertRed">
         {plan.message}
-      </p>
-    )
-  }
-
-  if (!categories.data) {
-    return (
-      <p className="mx-auto mt-32 text-center text-lg font-medium text-alertRed">
-        {categories.message}
       </p>
     )
   }
